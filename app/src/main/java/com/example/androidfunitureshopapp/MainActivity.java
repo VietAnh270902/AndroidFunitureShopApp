01cc611d9ff9a47b8025ec14a73ee92422fbb887
VietAnh270902 <95521603+VietAnh270902@users.noreply.github.com>
2023-03-11 16:37:10 +0700

    b comment

--- app/src/main/java/com/example/androidfunitureshopapp/MainActivity.java
+++ app/src/main/java/com/example/androidfunitureshopapp/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String name,the,girl,you,like;
-
+//        uk bạn ngủ chưa
     }
 }
