6b86a8c92babef69ecd5d147db80caed25d9a9d3
VietAnh270902 <95521603+VietAnh270902@users.noreply.github.com>
2023-03-11 16:17:20 +0700

    Initial commit

--- app/src/main/java/com/example/androidfunitureshopapp/MainActivity.java
+++ app/src/main/java/com/example/androidfunitureshopapp/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        alo hello
+        String name;
+        String things;
+        String you;
+        String me;
     }
 }
